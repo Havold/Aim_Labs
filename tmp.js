9b88e199bada46bc06efef25412bd2164a257881
Havold <21521479@gm.uit.edu.vn>
2024-06-02 17:25:17 +0700

    Add light

--- tmp.js
+++ tmp.js
@@ -7,6 +7,7 @@ import { createNoise2D } from "simplex-noise";
 import * as TWEEN from "@tweenjs/tween.js";
 
 var scene, camera, renderer, mesh;
+var ambientLight, light;
 var meshFloor;
 
 var keyboard = {};
@@ -18,23 +19,40 @@ function init() {
 
     mesh = new THREE.Mesh(
         new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshBasicMaterial({ color: 0xff4444, wireframe: false })
+        new THREE.MeshPhongMaterial({ color: 0xff4444, wireframe: false })
     );
     mesh.position.y += mesh.geometry.parameters.height / 2;
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
     scene.add(mesh);
 
     meshFloor = new THREE.Mesh(
         new THREE.PlaneGeometry(10, 10, 10, 10),
-        new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false })
+        new THREE.MeshPhongMaterial({ color: 0xffffff, wireframe: false })
     );
     meshFloor.rotation.x -= Math.PI / 2;
+    meshFloor.receiveShadow = true;
     scene.add(meshFloor);
+
+    ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    scene.add(ambientLight);
+
+    light = new THREE.PointLight(0xffffff, 30, 18);
+    light.position.set(-3, 6, -3);
+    light.castShadow = true;
+    light.shadow.camera.near = 0.1;
+    light.shadow.camera.far = 25;
+    scene.add(light);
 
     camera.position.set(0, player.height, -5);
     camera.lookAt(new THREE.Vector3(0, player.height, 0));
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.BasicShadowMap;
+
     document.body.appendChild(renderer.domElement);
     animate();
 }
@@ -45,21 +63,21 @@ function animate() {
     mesh.rotation.x += 0.01;
     mesh.rotation.y += 0.02;
 
-    if (keyboard[87]) {
+    if (keyboard[87]) { // W key
         camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
         camera.position.z -= -Math.cos(camera.rotation.y) * player.speed;
     }
-    if (keyboard[83]) {
+    if (keyboard[83]) { // S key
         camera.position.x += Math.sin(camera.rotation.y) * player.speed;
         camera.position.z += -Math.cos(camera.rotation.y) * player.speed;
     }
 
-    if (keyboard[65]) {
+    if (keyboard[65]) { // A key
         camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * player.speed;
         camera.position.z += -Math.cos(camera.rotation.y + Math.PI / 2) * player.speed;
     }
 
-    if (keyboard[68]) {
+    if (keyboard[68]) { // D key
         camera.position.x += Math.sin(camera.rotation.y - Math.PI / 2) * player.speed;
         camera.position.z += -Math.cos(camera.rotation.y - Math.PI / 2) * player.speed;
     }
